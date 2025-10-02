32a50bd3933534147a8a16a36bb7472249771ead
surajbkale <surajkalecsit@gmail.com>
2025-10-02 10:53:57 +0530

    line and circle added

--- apps/web/app/draw/page.tsx
+++ apps/web/app/draw/page.tsx
@@ -1,26 +1,46 @@
 "use client";
-import React, { useEffect } from "react";
-import { useRef } from "react";
+import React, { useState, useRef, useEffect, use } from "react";
 import { drawShape } from "../utils/drawshape";
 
 const page = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [shape, setShape] = useState<string>("line");
 
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
-      drawShape(canvas);
+      drawShape(canvas, shape);
     }
-  }, [canvasRef]);
+  }, [canvasRef, shape]);
 
   return (
     <div>
+      <div className="p-4 flex gap-2">
+        <button
+          onClick={() => setShape("react")}
+          className="px-2 py-1 bg-gray-500 rounded"
+        >
+          React
+        </button>
+        <button
+          onClick={() => setShape("arc")}
+          className="px-2 py-1 bg-gray-500 rounded"
+        >
+          Arc
+        </button>
+        <button
+          onClick={() => setShape("line")}
+          className="px-2 py-1 bg-gray-500 rounded"
+        >
+          Line
+        </button>
+      </div>
       <canvas
         ref={canvasRef}
-        height={580}
-        width={1280}
+        width={700}
+        height={1380}
         className="rgba(0, 0, 0)"
-      />
+      ></canvas>
     </div>
   );
 };
